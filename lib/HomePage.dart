cfd9acb5cc026f1692c316e06549c6c00ad08f87
Arthurlima544 <arthurlindaolima544@gmail.com>
2021-09-21 18:14:20 -0300

    adicionando scratch da homePage

---
+++ lib/HomePage.dart
@@ -0,0 +1,27 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Things"),
+      ),
+      body: ListView.builder(
+        itemCount: 3,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            padding: EdgeInsets.all(10),
+            child: ListTile(
+              title: Text("Teste"),
+              tileColor:
+                  Colors.primaries[Random().nextInt(Colors.accents.length)],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}

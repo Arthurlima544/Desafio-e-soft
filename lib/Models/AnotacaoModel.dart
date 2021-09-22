b2a742b266b315eccc55e194736de2dbc795c3f9
Arthurlima544 <arthurlindaolima544@gmail.com>
2021-09-22 07:15:51 -0300

    adicionando metodos from e tojson

--- lib/Models/AnotacaoModel.dart
+++ lib/Models/AnotacaoModel.dart
@@ -3,17 +3,34 @@ import 'package:flutter/cupertino.dart';
 
 @immutable
 class AnotacaoModel extends Equatable {
-  final String id;
-  final String titulo;
-  final String descricao;
-  final bool concluido;
+  late String id;
+  late String titulo;
+  late String descricao;
+  late bool concluido;
 
-  AnotacaoModel({
-    required this.id,
-    required this.titulo,
-    required this.descricao,
-    required this.concluido,
-  });
+  AnotacaoModel(
+    this.id,
+    this.titulo,
+    this.descricao,
+    this.concluido,
+  );
+
   @override
   List<Object?> get props => [titulo, descricao, concluido];
+
+  AnotacaoModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    titulo = json['titulo'];
+    descricao = json['descricao'];
+    concluido = json['concluido'] == 0 ? false : true;
+  }
+
+  Map<String, Object?> toJson() {
+    final Map<String, Object?> data = new Map<String, Object?>();
+    data['id'] = this.id;
+    data['titulo'] = this.titulo;
+    data['descricao'] = this.descricao;
+    data['concluido'] = this.concluido == false ? 0 : 1;
+    return data;
+  }
 }

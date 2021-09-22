f0aeeb38c4dab32fc7457d4339f09e3ad3cd50d8
Arthurlima544 <arthurlindaolima544@gmail.com>
2021-09-22 12:23:30 -0300

    implementando banco de dados

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -1,28 +1,58 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:app_anotacoes/Models/AnotacaoModel.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class AnotacaoDatabase {
-  static Database? _db;
-
-  Future<Database?> getDatabase() async {
-    if (_db == null) {
-      _db = await databaseInitialize();
-    }
-    return _db;
-  }
-
-  Future<Database> databaseInitialize() async {
+  Future<Database> database() async {
     Directory dir = await getApplicationDocumentsDirectory();
     String path = dir.path + 'anotacoes.db';
-    final database = await openDatabase(path, version: 1, onCreate: _createDb);
+    final database = openDatabase(
+      path,
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE anotacao(id TEXT PRIMARY KEY, titulo TEXT, descricao TEXT, concluido INTEGER)',
+        );
+      },
+      version: 1,
+    );
+
     return database;
   }
 
-  FutureOr<void> _createDb(Database db, int version) async {
-    await db.execute(
-        'CREATE TABLE anotacao(id TEXT PRIMARY KEY, titulo TEXT, descricao TEXT, concluido INTEGER)');
+  Future create(AnotacaoModel anotacaoModel) async {
+    final db = await AnotacaoDatabase().database();
+
+    await db.insert(
+      'anotacao',
+      anotacaoModel.toJson(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future update(AnotacaoModel anotacaoModel) async {
+    final db = await AnotacaoDatabase().database();
+
+    await db.update('anotacao', anotacaoModel.toJson(),
+        where: 'id = ?', whereArgs: [anotacaoModel.id]);
+  }
+
+  Future delete(String id) async {
+    final db = await AnotacaoDatabase().database();
+
+    await db.delete('anotacao', where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<List<AnotacaoModel>> getList() async {
+    final db = await AnotacaoDatabase().database();
+
+    final result = await db.query('anotacao');
+
+    final List<AnotacaoModel> anotacaoList =
+        result.map((e) => AnotacaoModel.fromJson(e)).toList();
+
+    return anotacaoList;
   }
 }

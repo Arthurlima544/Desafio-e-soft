1306afc5267c1bc9fc424b76e4eaa2afb15eb22c
Arthurlima544 <arthurlindaolima544@gmail.com>
2021-09-22 08:56:14 -0300

    adicionando mapEventToState

--- lib/Pages/Homepage/bloc/homepage_bloc.dart
+++ lib/Pages/Homepage/bloc/homepage_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:app_anotacoes/Models/AnotacaoModel.dart';
+import 'package:app_anotacoes/data/database.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -6,9 +7,17 @@ part 'homepage_event.dart';
 part 'homepage_state.dart';
 
 class HomepageBloc extends Bloc<HomepageEvent, HomepageState> {
-  HomepageBloc() : super(HomepageInitial()) {
-    on<HomepageEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  HomepageBloc(HomepageState initialState) : super(HomepageInitial());
+  Stream<HomepageState> mapEventToState(event) async* {
+    if (event is HomeFetchList) {
+      yield HomeLoadingState();
+    }
+  }
+
+  Stream<HomepageState> _fetchAnotacoes() async* {
+    try {
+      final List<AnotacaoModel> list = await AnotacaoDatabase.getAll();
+      yield HomeLoadedState(list: list);
+    } catch (e) {}
   }
 }
